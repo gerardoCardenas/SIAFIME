8e9f1b027a75e81800941b02abec852b7de4aa9a
Gerardo <Gerardo@DESKTOP-M211PR3>
2017-11-09 00:17:20 -0600

    Gerardo Cardenas
    Se sube la entidad de Vuelo

---
+++ SIA/src/entidades/Vuelo.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author Gerardo
+ * Clase: Vuelo
+ * 09/11/2017
+ */
+public class Vuelo {
+    private int idVuelo;
+    private String capacidad;
+    private String  modeloAvion;
+    private String numeroVuelo;
+    private int idConpañia;
+    private String escala;
+
+    public int getIdVuelo() {
+        return idVuelo;
+    }
+
+    public void setIdVuelo(int idVuelo) {
+        this.idVuelo = idVuelo;
+    }
+
+    public String getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(String capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getModeloAvion() {
+        return modeloAvion;
+    }
+
+    public void setModeloAvion(String modeloAvion) {
+        this.modeloAvion = modeloAvion;
+    }
+
+    public String getNumeroVuelo() {
+        return numeroVuelo;
+    }
+
+    public void setNumeroVuelo(String numeroVuelo) {
+        this.numeroVuelo = numeroVuelo;
+    }
+
+    public int getIdConpañia() {
+        return idConpañia;
+    }
+
+    public void setIdConpañia(int idConpañia) {
+        this.idConpañia = idConpañia;
+    }
+
+    public String getEscala() {
+        return escala;
+    }
+
+    public void setEscala(String escala) {
+        this.escala = escala;
+    }
+
+    public Vuelo(String capacidad, String modeloAvion, String numeroVuelo, int idConpañia, String escala) {
+        this.capacidad = capacidad;
+        this.modeloAvion = modeloAvion;
+        this.numeroVuelo = numeroVuelo;
+        this.idConpañia = idConpañia;
+        this.escala = escala;
+    }
+
+    public Vuelo() {
+    }
+}

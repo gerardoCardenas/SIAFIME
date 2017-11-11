dff84e72b83ad7828edf4343c9192d9a31ffea0a
Pablo I <Pablo I@DESKTOP-LLHRTM3>
2017-11-11 16:36:49 -0600

    Pablo Hernandez Se crea tabla de aerolineas el controlador mod

---
+++ SIA/src/controladores/AerolineaListController.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladores;
+
+import entidades.Aerolinea;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import lanzadores.AdministradorLaunch;
+import lanzadores.UsuarioListLaunch;
+
+/**
+ * FXML Controller class
+ *
+ * @author Pablo I
+ */
+public class AerolineaListController implements Initializable {
+
+    @FXML
+    private TableView<Aerolinea> tbAerolinea;
+    @FXML
+    private TableColumn<Aerolinea, Integer> tbcIdAerolinea;
+    @FXML
+    private TableColumn<Aerolinea, String> tbcNombre;
+    @FXML
+    private TableColumn<Aerolinea, String> tbcFMod;
+    @FXML
+    private TableColumn<Aerolinea, String> tbcModP;
+    @FXML
+    private TextField txtBuscar;
+    @FXML
+    private Button btnBuscar;
+    @FXML
+    private Button btnNuevo;
+
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //setTable();
+        //rellenar
+    }    
+
+    @FXML
+    private void Buscar(ActionEvent event) {
+    }
+
+    @FXML
+    private void Nuevo(ActionEvent event) {
+    }
+
+    @FXML
+    private void regresar(ActionEvent event) {
+        AdministradorLaunch al1 = new AdministradorLaunch();
+        al1.launch();
+        UsuarioListLaunch ull1 = new UsuarioListLaunch();
+        ull1.close();
+    }
+    
+}

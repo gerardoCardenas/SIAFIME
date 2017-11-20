8b61299825eceef75925e88d15e683e3e77132cd
Pablo I <Pablo I@DESKTOP-LLHRTM3>
2017-11-20 01:38:34 -0600

    Pablo Se agrega id y funcionalidad al controlador

--- SIA/src/controladores/RegistrarClienteController.java
+++ SIA/src/controladores/RegistrarClienteController.java
@@ -5,12 +5,21 @@
  */
 package controladores;
 
+import entidades.Cliente;
+import facade.ClienteFacade;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javax.swing.JOptionPane;
+import lanzadores.ClienteListLaunch;
+import lanzadores.RegistrarClienteLaunch;
 
 /**
  * FXML Controller class
@@ -34,14 +43,62 @@ public class RegistrarClienteController implements Initializable {
     @FXML
     private Button btnRegistrar;
     @FXML
-    private TextField txfPassword;
+    private Hyperlink back;
+    @FXML
+    private RadioButton rbtnF;
+    @FXML
+    private ToggleGroup Gen;
+    @FXML
+    private RadioButton rbtnM;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        Cliente c = new Cliente();
+        if(ClienteFacade.getEstado().equals("Registrar")){
+            btnRegistrar.setText("Registrar");
+            txfNombre.setText("");
+            txfApellidos.setText("");
+            txfEdad.setText("");
+            rbtnF.setSelected(true);
+            txfTelefono.setText("");
+            txfDireccion.setText("");
+            txfCorreo.setText("");
+        }
     }    
-    
+
+    @FXML
+    private void salir(ActionEvent event) {
+        ClienteListLaunch cll = new ClienteListLaunch();
+        cll.launch();
+        RegistrarClienteLaunch rcl = new RegistrarClienteLaunch();
+        rcl.close();
+    }
+
+    @FXML
+    private void guardar(ActionEvent event) {
+        Cliente c = new Cliente();
+        c.setNombre(txfNombre.getText());
+        c.setApellido(txfApellidos.getText());
+        c.setEdad(Integer.parseInt(txfEdad.getText()));
+        if(rbtnF.isSelected()){
+            c.setGenero("F");
+        }else if(rbtnM.isSelected()){
+            c.setGenero("M");
+        }
+        c.setTelefono(txfTelefono.getText());
+        c.setDireccion(txfDireccion.getText());
+        c.setEmail(txfCorreo.getText());
+        try{
+            c.insert();
+            ClienteListLaunch cll = new ClienteListLaunch();
+            cll.launch();
+            RegistrarClienteLaunch rcl = new RegistrarClienteLaunch();
+            rcl.close();
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Error en el llenado de datos");
+        }
+    }
 }

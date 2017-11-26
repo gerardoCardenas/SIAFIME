c161fccd64cd98830cd0d926fd0cc94fd4c04c97
Pablo I <Pablo I@DESKTOP-LLHRTM3>
2017-11-26 15:16:31 -0600

    Pablo Se crea ventana Vuelo 27/11/2017

---
+++ SIA/src/lanzadores/VueloListLaunch.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lanzadores;
+
+import db.Sesion;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Pablo I
+ */
+public class VueloListLaunch {
+    public static Stage stage;
+    public void launch(){
+        try {
+            stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Vuelos.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.close();
+            stage.setTitle("Vuelos");
+            stage.setScene(new Scene(root1));  
+            stage.show();
+            stage.centerOnScreen();
+            stage.setResizable(false);
+            Sesion s1 = new Sesion();
+        } catch (Exception e) {
+        }
+    }
+    public void close(){
+        stage.close();
+        stage = new Stage();
+    }
+}
